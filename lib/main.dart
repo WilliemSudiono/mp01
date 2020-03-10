7a26f1b506757c472df43e18b66efa50f098a15e
Williem <williem@invosa.com>
2020-03-10 16:18:44 +0700

    ganti teks

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                'Cikarang',
+                'Jakarta',
                 style: TextStyle(color: Colors.grey[500]),
               )
             ],
